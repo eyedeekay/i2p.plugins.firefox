f7a62f57cc2aeda37c5cd2bd168b3751662f4c8d
idk <hankhill19580@gmail.com>
2022-08-07 21:29:26 -0400

    bump version

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ GITHUB_USER=eyedeekay
 GITHUB_REPO=i2p.plugins.firefox
 GITHUB_NAME="Initial Release"
 GITHUB_DESCRIPTION=$(cat README.md)
-GITHUB_TAG=0.0.2
+GITHUB_TAG=0.0.3
 ant distclean
 cd src && \
     ant
