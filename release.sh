5ea329554f5200a90d9dd69d49cf8cf3bf74ed25
idk <hankhill19580@gmail.com>
2022-08-07 20:49:20 -0400

    update version
    Former-commit-id: b08743d09b57e51208c4c698f601998e7aa42e5b
    Former-commit-id: 43a23b482c887e69ef83f953040ec93a98744be6

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ GITHUB_USER=eyedeekay
 GITHUB_REPO=i2p.plugins.firefox
 GITHUB_NAME="Initial Release"
 GITHUB_DESCRIPTION=$(cat README.md)
-GITHUB_TAG=0.0.1
+GITHUB_TAG=0.0.2
 ant distclean
 cd src && \
     ant
@@ -22,3 +22,9 @@ github-release upload --user "${GITHUB_USER}" \
     --replace
 cd ../
 zip -r i2pfirefox.zip src/build/i2pfirefox.jar i2pbrowser.cmd LICENSE.md README.md
+github-release upload --user "${GITHUB_USER}" \
+    --repo "${GITHUB_REPO}" \
+    --tag "${GITHUB_TAG}" \
+    --name "i2pfirefox.zip" \
+    --file "i2pfirefox.zip" \
+    --replace   
