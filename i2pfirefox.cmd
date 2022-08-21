2875197bc81c44b5e114f5bf24f22fbc2ee4dba5
idk <hankhill19580@gmail.com>
2022-08-20 20:10:18 -0400

    add chromium launcher-only script

---
+++ i2pfirefox.cmd
@@ -0,0 +1,3 @@
+:; dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd); java -cp "$dir"/src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PChromium; exit $?
+@ECHO OFF
+java -cp %cd%/src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PChromium
