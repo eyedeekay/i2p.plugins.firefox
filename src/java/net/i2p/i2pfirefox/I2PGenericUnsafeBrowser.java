539e3340b4573af648a5deb4810f5fc5ad3339bb
idk <hankhill19580@gmail.com>
2022-08-27 19:50:32 -0400

    update index.html
    Former-commit-id: cc54b8518816dcbfdbc74cf227953fe313186367
    Former-commit-id: a9c52d1c5d5a9f8f346f259a7d6e195a9eec5248

---
+++ src/java/net/i2p/i2pfirefox/I2PGenericUnsafeBrowser.java
@@ -0,0 +1,26 @@
+package net.i2p.i2pfirefox;
+
+/**
+ * I2PChromiumProfileChecker.java
+ * Copyright (C) 2022 idk <hankhill19580@gmail.com>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License. See LICENSE.md for details.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * I2PGenericUnsafeBrowser is a wrapper which sets common environment
+ * variables for the process controlled by a processbuilder.
+ * 
+ * @author idk
+ * @since 0.0.18
+ */
+
+public class I2PGenericUnsafeBrowser {
+    private final String unsafeBrowserPath;
+    I2PGenericUnsafeBrowser() {
+        unsafeBrowserPath = "";
+    }
+    
+}
