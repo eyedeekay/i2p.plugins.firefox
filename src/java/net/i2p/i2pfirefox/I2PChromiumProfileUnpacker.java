48ba47b274d011c2ee51e791d38c687d954c384f
idk <hankhill19580@gmail.com>
2022-08-30 14:22:47 -0400

    fix unaltered FIREFOX vars in I2PChromiumProfileBuilder.java

--- src/java/net/i2p/i2pfirefox/I2PChromiumProfileUnpacker.java
+++ src/java/net/i2p/i2pfirefox/I2PChromiumProfileUnpacker.java
@@ -24,7 +24,7 @@ import java.util.zip.ZipInputStream;
  * @author idk
  * @since 0.0.1
  */
-public class I2PChromiumProfileUnpacker {
+public class I2PChromiumProfileUnpacker extends I2PCommonBrowser {
 
     public static void main(String[] args) {
         String profileDirectory = I2PChromiumProfileBuilder.profileDirectory();
@@ -43,45 +43,7 @@ public class I2PChromiumProfileUnpacker {
      */
     public boolean unpackProfile(String profileDirectory) {
         System.out.println("Unpacking base profile to " + profileDirectory);
-        try {
-            final InputStream resources = this.getClass().getClassLoader().getResourceAsStream("i2p.chromium.base.profile.zip");
-            if (resources == null) {
-                System.out.println("Could not find resources");
-                return false;
-            }
-            System.out.println(resources.toString());
-            // InputStream corresponds to a zip file. Unzip it.
-            //Files.copy(r, new File(profileDirectory).toPath(), StandardCopyOption.REPLACE_EXISTING);
-            ZipInputStream zis = new ZipInputStream(resources);
-            ZipEntry entry;
-                // while there are entries I process them
-            while ((entry = zis.getNextEntry()) != null)
-            {
-                System.out.println("entry: " + entry.getName() + ", " + entry.getSize());
-                // consume all the data from this entry
-                if (entry.isDirectory()) {
-                    System.out.println("Creating directory: " + entry.getName());
-                    File dir = new File(profileDirectory + "/" + entry.getName());
-                    dir.mkdirs();
-                } else {
-                    System.out.println("Creating file: " + entry.getName());
-                    File file = new File(profileDirectory + "/" + entry.getName());
-                    file.createNewFile();
-                    Files.copy(zis, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                }
-                while (zis.available() > 0)
-                zis.read();
-                    // I could close the entry, but getNextEntry does it automatically
-                    // zis.closeEntry()
-            }
-            // loop through the Enumeration
-            
-        } catch (Exception e) {
-            System.out.println("Error copying profile files: " + e.getMessage());
-            return false;
-        }
-        return true;
+        return unpackProfile(profileDirectory, "chromium", "base");
     }
-    
     
 }
--- src/java/net/i2p/i2pfirefox/I2PCommonBrowser.java
+++ src/java/net/i2p/i2pfirefox/I2PCommonBrowser.java
@@ -1,6 +1,11 @@
 package net.i2p.i2pfirefox;
 
 import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class I2PCommonBrowser {
     
@@ -67,5 +72,47 @@ public class I2PCommonBrowser {
             }
         }
         return "";
+    }
+    
+    public boolean unpackProfile(String profileDirectory, String browser, String base) {
+        System.out.println("Unpacking base profile to " + profileDirectory);
+        try {
+            final InputStream resources = this.getClass().getClassLoader().getResourceAsStream("i2p."+browser+"."+base+".profile.zip");
+            if (resources == null) {
+                System.out.println("Could not find resources");
+                return false;
+            }
+            System.out.println(resources.toString());
+            // InputStream corresponds to a zip file. Unzip it.
+            //Files.copy(r, new File(profileDirectory).toPath(), StandardCopyOption.REPLACE_EXISTING);
+            ZipInputStream zis = new ZipInputStream(resources);
+            ZipEntry entry;
+                // while there are entries I process them
+            while ((entry = zis.getNextEntry()) != null)
+            {
+                System.out.println("entry: " + entry.getName() + ", " + entry.getSize());
+                // consume all the data from this entry
+                if (entry.isDirectory()) {
+                    System.out.println("Creating directory: " + entry.getName());
+                    File dir = new File(profileDirectory + "/" + entry.getName());
+                    dir.mkdirs();
+                } else {
+                    System.out.println("Creating file: " + entry.getName());
+                    File file = new File(profileDirectory + "/" + entry.getName());
+                    file.createNewFile();
+                    Files.copy(zis, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                }
+                while (zis.available() > 0)
+                zis.read();
+                    // I could close the entry, but getNextEntry does it automatically
+                    // zis.closeEntry()
+            }
+            // loop through the Enumeration
+            
+        } catch (Exception e) {
+            System.out.println("Error copying profile files: " + e.getMessage());
+            return false;
+        }
+        return true;
     }
 }
