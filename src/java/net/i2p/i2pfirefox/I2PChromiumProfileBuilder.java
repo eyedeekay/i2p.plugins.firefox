32e5dd112f1864e92b38177276b5b7c82266dd85
idk <hankhill19580@gmail.com>
2022-08-30 15:49:44 -0400

    clangfmt everything
    Former-commit-id: 84bc6480d90b750cd0190b2cae0ecfa556fe14c9
    Former-commit-id: fa1c9c8f207bc52eb2e8fa9c1d643bea0b17af4f

--- src/java/net/i2p/i2pfirefox/I2PChromiumProfileBuilder.java
+++ src/java/net/i2p/i2pfirefox/I2PChromiumProfileBuilder.java
@@ -7,7 +7,7 @@ import java.io.File;
  * Copyright (C) 2022 idk <hankhill19580@gmail.com>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the MIT License. See LICENSE.md for details.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
@@ -16,134 +16,135 @@ import java.io.File;
  * contains the I2P browser profile for the Chromium browser family. It manages
  * the base profile directory and copies it's contents to the active profile
  * directory, which is actually used by Chromium.
- * 
+ *
  * @author idk
  * @since 0.0.1
  */
 public class I2PChromiumProfileBuilder extends I2PCommonBrowser {
-    //private static boolean strict;
+  // private static boolean strict;
 
-    /**
-     * get the profile directory, creating it if necessary
-     *
-     * @return the profile directory, or null if it could not be created
-     */
-    public static String profileDirectory() {
-        return profileDirectory("I2P_CHROMIUM_PROFILE", "chromium");
+  /**
+   * get the profile directory, creating it if necessary
+   *
+   * @return the profile directory, or null if it could not be created
+   */
+  public static String profileDirectory() {
+    return profileDirectory("I2P_CHROMIUM_PROFILE", "chromium");
+  }
+
+  private static String baseProfileDir(String file) {
+    File profileDir = new File(file, "i2p.chromium.base.profile");
+    // make sure the directory exists
+    if (profileDir.exists()) {
+      return profileDir.getAbsolutePath();
+    } else {
+      // create the directory
+      I2PChromiumProfileUnpacker unpacker = new I2PChromiumProfileUnpacker();
+      if (!unpacker.unpackProfile(profileDir.getAbsolutePath())) {
+        return null;
+      }
+      return profileDir.getAbsolutePath();
     }
-    
-    private static String baseProfileDir(String file) {
-        File profileDir = new File(file, "i2p.chromium.base.profile");
-        // make sure the directory exists
-        if (profileDir.exists()) {
-            return profileDir.getAbsolutePath();
-        } else {
-            // create the directory
-            I2PChromiumProfileUnpacker unpacker = new I2PChromiumProfileUnpacker();
-            if (!unpacker.unpackProfile(profileDir.getAbsolutePath())) {
-                return null;
-            }
-            return profileDir.getAbsolutePath();
+  }
+
+  /**
+   * get the base profile directory, creating it if necessary
+   *
+   * @return the base profile directory, or null if it could not be created
+   */
+  public static String baseProfileDirectory() {
+    String pd = System.getenv("I2P_CHROMIUM_BASE_PROFILE");
+    if (pd != null && !pd.isEmpty()) {
+      File pdf = new File(pd);
+      if (pdf.exists() && pdf.isDirectory()) {
+        return pd;
+      } else {
+        I2PChromiumProfileUnpacker unpacker = new I2PChromiumProfileUnpacker();
+        if (!unpacker.unpackProfile(pdf.getAbsolutePath())) {
+          return null;
         }
+      }
     }
+    String rtd = runtimeDirectory();
+    return baseProfileDir(rtd);
+  }
 
-    /**
-     * get the base profile directory, creating it if necessary
-     *
-     * @return the base profile directory, or null if it could not be created
-     */
-    public static String baseProfileDirectory() {
-        String pd = System.getenv("I2P_CHROMIUM_BASE_PROFILE");
-        if (pd != null && !pd.isEmpty()) {
-            File pdf = new File(pd);
-            if (pdf.exists() && pdf.isDirectory()) {
-                return pd;
-            }else{
-                I2PChromiumProfileUnpacker unpacker = new I2PChromiumProfileUnpacker();
-                if (!unpacker.unpackProfile(pdf.getAbsolutePath())) {
-                    return null;
-                }
-            }
-        }
-        String rtd = runtimeDirectory();
-        return baseProfileDir(rtd);
-    }
+  /**
+   * get the runtime directory, creating it if create=true
+   *
+   * @param create if true, create the runtime directory if it does not exist
+   * @return the runtime directory, or null if it could not be created
+   * @since 0.0.1
+   */
+  public static File runtimeDirectory(boolean create) {
+    String rtd = runtimeDirectory();
+    return runtimeDirectory(create, rtd);
+  }
 
-    /**
-     * get the runtime directory, creating it if create=true
-     * 
-     * @param create if true, create the runtime directory if it does not exist
-     * @return the runtime directory, or null if it could not be created
-     * @since 0.0.1
-     */
-    public static File runtimeDirectory(boolean create) {
-        String rtd = runtimeDirectory();
-        return runtimeDirectory(create, rtd);
+  /**
+   * get the correct runtime directory
+   *
+   * @return the runtime directory, or null if it could not be created or found
+   * @since 0.0.1
+   */
+  public static String runtimeDirectory() {
+    // get the I2P_CHROMIUM_DIR environment variable
+    String rtd = System.getenv("I2P_CHROMIUM_DIR");
+    // if it is not null and not empty
+    if (rtd != null && !rtd.isEmpty()) {
+      // check if the file exists
+      File rtdFile = new File(rtd);
+      if (rtdFile.exists()) {
+        // if it does, return it
+        return runtimeDirectory(rtd);
+      }
     }
+    return runtimeDirectory("");
+  }
 
-    /**
-     * get the correct runtime directory
-     * 
-     * @return the runtime directory, or null if it could not be created or found
-     * @since 0.0.1
-     */
-    public static String runtimeDirectory() {
-        // get the I2P_CHROMIUM_DIR environment variable
-        String rtd = System.getenv("I2P_CHROMIUM_DIR");
-        // if it is not null and not empty
-        if (rtd != null && !rtd.isEmpty()) {
-            // check if the file exists
-            File rtdFile = new File(rtd);
-            if (rtdFile.exists()) {
-                // if it does, return it
-                return runtimeDirectory(rtd);
-            }
-        }
-        return runtimeDirectory("");
+  /**
+   * Copy the inert base profile directory to the runtime profile directory
+   *
+   * @since 0.0.1
+   */
+  public static boolean copyBaseProfiletoProfile() {
+    String baseProfile = baseProfileDirectory();
+    String profile = profileDirectory();
+    System.out.println("Copying base profile to profile directory: " +
+                       baseProfile + " -> " + profile);
+    if (baseProfile.isEmpty() || profile.isEmpty()) {
+      return false;
     }
+    File baseProfileDir = new File(baseProfile);
+    File profileDir = new File(profile);
 
-    /**
-     * Copy the inert base profile directory to the runtime profile directory
-     *
-     * @since 0.0.1
-     */
-    public static boolean copyBaseProfiletoProfile() {
-        String baseProfile = baseProfileDirectory();
-        String profile = profileDirectory();
-        System.out.println("Copying base profile to profile directory: " + baseProfile + " -> " + profile);
-        if (baseProfile.isEmpty() || profile.isEmpty()) {
-            return false;
-        }
-        File baseProfileDir = new File(baseProfile);
-        File profileDir = new File(profile);
-        
-        try {
-            System.out.println("Copying base profile to profile directory");
-            copyDirectory(baseProfileDir, profileDir);
-        } catch (Exception e) {
-            System.out.println("Error copying base profile to profile"+e);
-            return false;
-        }
-        System.out.println("Copied base profile to profile directory");        
-        return true;
+    try {
+      System.out.println("Copying base profile to profile directory");
+      copyDirectory(baseProfileDir, profileDir);
+    } catch (Exception e) {
+      System.out.println("Error copying base profile to profile" + e);
+      return false;
     }
+    System.out.println("Copied base profile to profile directory");
+    return true;
+  }
 
-    /**
-     * Construct a new Profile Builder
-     * 
-     * @since 0.0.1
-     */
-    public I2PChromiumProfileBuilder() {
-        //I2PChromiumProfileBuilder.strict = false;
-    }
+  /**
+   * Construct a new Profile Builder
+   *
+   * @since 0.0.1
+   */
+  public I2PChromiumProfileBuilder() {
+    // I2PChromiumProfileBuilder.strict = false;
+  }
 
-    /**
-     * Construct a new Profile Builder
-     * @param strict if true, the strict overrides will be copied to the profile
-     * 
-     * @since 0.0.1
-     */
-    public I2PChromiumProfileBuilder(boolean strict) {
-        //I2PChromiumProfileBuilder.strict = strict;
-    }
+  /**
+   * Construct a new Profile Builder
+   * @param strict if true, the strict overrides will be copied to the profile
+   *
+   * @since 0.0.1
+   */
+  public I2PChromiumProfileBuilder(boolean strict) {
+    // I2PChromiumProfileBuilder.strict = strict;
+  }
 }
--- src/java/net/i2p/i2pfirefox/I2PCommonBrowser.java
+++ src/java/net/i2p/i2pfirefox/I2PCommonBrowser.java
@@ -12,162 +12,173 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class I2PCommonBrowser {
-    
-    /**
-     * get the runtime directory, creating it if create=true
-     * 
-     * @param create if true, create the runtime directory if it does not exist
-     * @return the runtime directory, or null if it could not be created
-     * @since 0.0.19
-     */
-    protected static File runtimeDirectory(boolean create, String override) {
-        String rtd = runtimeDirectory(override);
-        File rtdFile = new File(rtd);
-        if (create) {
-            if (!rtdFile.exists()) {
-                rtdFile.mkdir();
-            }
-        }
-        return new File(rtd);
-    }
 
-    /**
-     * get the correct runtime directory
-     * 
-     * @return the runtime directory, or null if it could not be created or found
-     * @since 0.0.19
-     */
-    protected static String runtimeDirectory(String override) {
-        // get the I2P_FIREFOX_DIR environment variable
-        String rtd = System.getenv(override);
-        // if it is not null and not empty
-        if (rtd != null && !rtd.isEmpty()) {
-            // check if the file exists
-            File rtdFile = new File(rtd);
-            if (rtdFile.exists()) {
-                // if it does, return it
-                return rtd;
-            }
-        }
-        // obtain the PLUGIN environment variable
-        String plugin = System.getenv("PLUGIN");
-        if (plugin != null && !plugin.isEmpty()) {
-            File pluginDir = new File(plugin);
-            if (pluginDir.exists()) {
-                return pluginDir.toString();
-            }
-        }
-        String userDir = System.getProperty("user.dir");
-        if (userDir != null && !userDir.isEmpty()) {
-            File userDir1 = new File(userDir);
-            if (userDir1.exists()) {
-                return userDir1.toString();
-            }
-        }
-        String homeDir = System.getProperty("user.home");
-        if (homeDir != null && !homeDir.isEmpty()) {
-            File homeDir1 = new File(homeDir+"/.i2p");
-            if (homeDir1.exists()) {
-                return homeDir.toString();
-            }
-            File homeDir2 = new File(homeDir+"/i2p");
-            if (homeDir2.exists()) {
-                return homeDir2.toString();
-            }
-        }
-        return "";
+  /**
+   * get the runtime directory, creating it if create=true
+   *
+   * @param create if true, create the runtime directory if it does not exist
+   * @return the runtime directory, or null if it could not be created
+   * @since 0.0.19
+   */
+  protected static File runtimeDirectory(boolean create, String override) {
+    String rtd = runtimeDirectory(override);
+    File rtdFile = new File(rtd);
+    if (create) {
+      if (!rtdFile.exists()) {
+        rtdFile.mkdir();
+      }
     }
+    return new File(rtd);
+  }
 
-    /**
-     * get the profile directory, creating it if necessary
-     *
-     * @return the profile directory, or null if it could not be created
-     * @since 0.0.19
-     */
-    protected static String profileDirectory(String envVar, String browser) {
-        String pd = System.getenv(envVar);
-        if (pd != null && !pd.isEmpty()) {
-            File pdf = new File(pd);
-            if (pdf.exists() && pdf.isDirectory()) {
-                return pd;
-            }
-        }
-        String rtd = runtimeDirectory("");
-        return profileDir(rtd, browser);
+  /**
+   * get the correct runtime directory
+   *
+   * @return the runtime directory, or null if it could not be created or found
+   * @since 0.0.19
+   */
+  protected static String runtimeDirectory(String override) {
+    // get the I2P_FIREFOX_DIR environment variable
+    String rtd = System.getenv(override);
+    // if it is not null and not empty
+    if (rtd != null && !rtd.isEmpty()) {
+      // check if the file exists
+      File rtdFile = new File(rtd);
+      if (rtdFile.exists()) {
+        // if it does, return it
+        return rtd;
+      }
     }
+    // obtain the PLUGIN environment variable
+    String plugin = System.getenv("PLUGIN");
+    if (plugin != null && !plugin.isEmpty()) {
+      File pluginDir = new File(plugin);
+      if (pluginDir.exists()) {
+        return pluginDir.toString();
+      }
+    }
+    String userDir = System.getProperty("user.dir");
+    if (userDir != null && !userDir.isEmpty()) {
+      File userDir1 = new File(userDir);
+      if (userDir1.exists()) {
+        return userDir1.toString();
+      }
+    }
+    String homeDir = System.getProperty("user.home");
+    if (homeDir != null && !homeDir.isEmpty()) {
+      File homeDir1 = new File(homeDir + "/.i2p");
+      if (homeDir1.exists()) {
+        return homeDir.toString();
+      }
+      File homeDir2 = new File(homeDir + "/i2p");
+      if (homeDir2.exists()) {
+        return homeDir2.toString();
+      }
+    }
+    return "";
+  }
 
-    protected static String profileDir(String file, String browser) {
-        File profileDir = new File(file, "i2p."+browser+".profile");
-        return profileDir.getAbsolutePath();
+  /**
+   * get the profile directory, creating it if necessary
+   *
+   * @return the profile directory, or null if it could not be created
+   * @since 0.0.19
+   */
+  protected static String profileDirectory(String envVar, String browser) {
+    String pd = System.getenv(envVar);
+    if (pd != null && !pd.isEmpty()) {
+      File pdf = new File(pd);
+      if (pdf.exists() && pdf.isDirectory()) {
+        return pd;
+      }
     }
+    String rtd = runtimeDirectory("");
+    return profileDir(rtd, browser);
+  }
 
-    protected boolean unpackProfile(String profileDirectory, String browser, String base) {
-        System.out.println("Unpacking base profile to " + profileDirectory);
-        try {
-            final InputStream resources = this.getClass().getClassLoader().getResourceAsStream("i2p."+browser+"."+base+".profile.zip");
-            if (resources == null) {
-                System.out.println("Could not find resources");
-                return false;
-            }
-            System.out.println(resources.toString());
-            // InputStream corresponds to a zip file. Unzip it.
-            //Files.copy(r, new File(profileDirectory).toPath(), StandardCopyOption.REPLACE_EXISTING);
-            ZipInputStream zis = new ZipInputStream(resources);
-            ZipEntry entry;
-                // while there are entries I process them
-            while ((entry = zis.getNextEntry()) != null)
-            {
-                System.out.println("entry: " + entry.getName() + ", " + entry.getSize());
-                // consume all the data from this entry
-                if (entry.isDirectory()) {
-                    System.out.println("Creating directory: " + entry.getName());
-                    File dir = new File(profileDirectory + "/" + entry.getName());
-                    dir.mkdirs();
-                } else {
-                    System.out.println("Creating file: " + entry.getName());
-                    File file = new File(profileDirectory + "/" + entry.getName());
-                    file.createNewFile();
-                    Files.copy(zis, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                }
-                while (zis.available() > 0)
-                zis.read();
-                    // I could close the entry, but getNextEntry does it automatically
-                    // zis.closeEntry()
-            }
-            // loop through the Enumeration
-            
-        } catch (Exception e) {
-            System.out.println("Error copying profile files: " + e.getMessage());
-            return false;
-        }
-        return true;
-    }
-    protected static void copyDirectory(File sourceDirectory, File destinationDirectory) throws IOException {
-        destinationDirectory = new File(destinationDirectory.toString().replace("i2p.chromium.base.profile", ""));
-        if (!destinationDirectory.exists()) {
-            destinationDirectory.mkdir();
-        }
-        for (String f : sourceDirectory.list()) {
-            copyDirectoryCompatibityMode(new File(sourceDirectory, f), new File(destinationDirectory, f));
-        }
-    }
+  protected static String profileDir(String file, String browser) {
+    File profileDir = new File(file, "i2p." + browser + ".profile");
+    return profileDir.getAbsolutePath();
+  }
 
-    public static void copyDirectoryCompatibityMode(File source, File destination) throws IOException {
-        if (source.isDirectory()) {
-            copyDirectory(source, destination);
+  protected boolean unpackProfile(String profileDirectory, String browser,
+                                  String base) {
+    System.out.println("Unpacking base profile to " + profileDirectory);
+    try {
+      final InputStream resources =
+          this.getClass().getClassLoader().getResourceAsStream(
+              "i2p." + browser + "." + base + ".profile.zip");
+      if (resources == null) {
+        System.out.println("Could not find resources");
+        return false;
+      }
+      System.out.println(resources.toString());
+      // InputStream corresponds to a zip file. Unzip it.
+      // Files.copy(r, new File(profileDirectory).toPath(),
+      // StandardCopyOption.REPLACE_EXISTING);
+      ZipInputStream zis = new ZipInputStream(resources);
+      ZipEntry entry;
+      // while there are entries I process them
+      while ((entry = zis.getNextEntry()) != null) {
+        System.out.println("entry: " + entry.getName() + ", " +
+                           entry.getSize());
+        // consume all the data from this entry
+        if (entry.isDirectory()) {
+          System.out.println("Creating directory: " + entry.getName());
+          File dir = new File(profileDirectory + "/" + entry.getName());
+          dir.mkdirs();
         } else {
-            copyFile(source, destination);
+          System.out.println("Creating file: " + entry.getName());
+          File file = new File(profileDirectory + "/" + entry.getName());
+          file.createNewFile();
+          Files.copy(zis, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
         }
-    }
+        while (zis.available() > 0)
+          zis.read();
+        // I could close the entry, but getNextEntry does it automatically
+        // zis.closeEntry()
+      }
+      // loop through the Enumeration
 
-    private static void copyFile(File sourceFile, File destinationFile) throws IOException {
-        try (InputStream in = new FileInputStream(sourceFile); 
-            OutputStream out = new FileOutputStream(destinationFile)) {
-            byte[] buf = new byte[1024];
-            int length;
-            while ((length = in.read(buf)) > 0) {
-                out.write(buf, 0, length);
-            }
-        }
+    } catch (Exception e) {
+      System.out.println("Error copying profile files: " + e.getMessage());
+      return false;
     }
+    return true;
+  }
+
+  protected static void copyDirectory(File sourceDirectory,
+                                      File destinationDirectory)
+      throws IOException {
+    destinationDirectory = new File(destinationDirectory.toString().replace(
+        "i2p.chromium.base.profile", ""));
+    if (!destinationDirectory.exists()) {
+      destinationDirectory.mkdir();
+    }
+    for (String f : sourceDirectory.list()) {
+      copyDirectoryCompatibityMode(new File(sourceDirectory, f),
+                                   new File(destinationDirectory, f));
+    }
+  }
+
+  public static void copyDirectoryCompatibityMode(File source, File destination)
+      throws IOException {
+    if (source.isDirectory()) {
+      copyDirectory(source, destination);
+    } else {
+      copyFile(source, destination);
+    }
+  }
+
+  private static void copyFile(File sourceFile, File destinationFile)
+      throws IOException {
+    try (InputStream in = new FileInputStream(sourceFile);
+         OutputStream out = new FileOutputStream(destinationFile)) {
+      byte[] buf = new byte[1024];
+      int length;
+      while ((length = in.read(buf)) > 0) {
+        out.write(buf, 0, length);
+      }
+    }
+  }
 }
