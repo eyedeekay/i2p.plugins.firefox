464922f8c79dfb3db1b477d0a4a0558f0213b203
idk <hankhill19580@gmail.com>
2022-08-30 17:25:23 -0400

    add usability mode for Firefox
    Former-commit-id: 06239a90d2e697e9b08cfe324cbd28b1ad49e2ec
    Former-commit-id: 154ef9644bc029332eeff58165d1ab20774c8e61

--- test.sh
+++ test.sh
@@ -48,6 +48,21 @@ java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -firefox -priv
 
 echo "Firefox tests completed"
 sleep 2s
+rm -rf i2p.firefox.base.profile i2p.firefox.profile
+
+echo "Testing Firefox with no private and no URL parameters."
+java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -firefox -usability 2> fox.0.err 1> fox.0.log
+echo "Testing Firefox with local URL parameter."
+java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -firefox -usability "http://127.0.0.1:7657" 2> fox.1.err 1> fox.1.log
+echo "Testing Firefox with remote URL parameter."
+java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -firefox -usability "http://idk.i2p" 2> fox.2.err 1> fox.2.log
+echo "Testing Firefox with remote AND local URL parameter."
+java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -firefox -usability "http://127.0.0.1:7657" "http://idk.i2p" 2> fox.3.err 1> fox.3.log
+echo "Testing Firefox with private browsing parameter"
+java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -firefox -usability -private "http://127.0.0.1:7657" 2> fox.4.err 1> fox.4.log
+
+echo "Firefox Usability-Mode tests completed"
+sleep 2s
 rm -rf i2p.chromium.base.profile i2p.chromium.profile i2p.firefox.base.profile i2p.firefox.profile
 
 echo "Testing UNSAFE auto-selector with no private and no URL parameters."
