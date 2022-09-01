f462bf3c3b6211a751efdf26a2040bc9d62dea79
idk <hankhill19580@gmail.com>
2022-09-01 15:11:48 -0400

    jpackage it. Why the heck not.

--- release.sh
+++ release.sh
@@ -7,8 +7,10 @@ GITHUB_DESCRIPTION=$(cat CHANGES.md)
 GITHUB_TAG=0.0.18
 ant distclean
 ./javadoc.sh
+NUMLINE=`grep release.number build.xml | head -n 1`
+sed -i "s|$NUMLINE|        <property name=\"release.number\" value=\"$GITHUB_TAG\" />|g" build.xml
 edgar && git push --all
-ant jar freeZip
+ant jar freeZip jpackage
 github-release release --user "${GITHUB_USER}" \
     --repo "${GITHUB_REPO}" \
     --name "${GITHUB_NAME}" \
@@ -23,14 +25,23 @@ github-release edit --user "${GITHUB_USER}" \
 github-release upload --user "${GITHUB_USER}" \
     --repo "${GITHUB_REPO}" \
     --tag "${GITHUB_TAG}" \
+    --label "I2P Browser launcher as a .jar." \
     --name "i2pfirefox.jar" \
     --file "src/build/i2pfirefox.jar" \
     --replace
 github-release upload --user "${GITHUB_USER}" \
     --repo "${GITHUB_REPO}" \
     --tag "${GITHUB_TAG}" \
+    --label "I2P Browser launcher as a .jar and a set of semi-univeral launcher scripts." \
     --name "i2pfirefox.zip" \
     --file "i2pfirefox.zip" \
-    --replace   
+    --replace
+github-release upload --user "${GITHUB_USER}" \
+    --repo "${GITHUB_REPO}" \
+    --tag "${GITHUB_TAG}" \
+    --name "i2pbrowser-nojava.zip" \
+    --label "I2P Browser launcher as a Jpackage, does not require a JVM, Linux Only for now unless you BYO." \
+    --file "i2pbrowser.zip" \
+    --replace  
 git pull github --tags
 git push --all
