553472a0454c9317b2f87eb60d2ea532f9dbe61c
idk <hankhill19580@gmail.com>
2022-09-05 18:43:02 -0400

    make everything distributable better
    Former-commit-id: 7dbfdd9cbf0592e50adfa190289c13eea1119d84
    Former-commit-id: 1c089bbfa57285f4ba3e919f537e661088d172bf

--- debian.sh
+++ debian.sh
@@ -3,6 +3,11 @@
 . ./config.sh
 jpackage --verbose \
     --type deb \
+    --linux-deb-maintainer hankhill19580@gmail.com \
+    --linux-menu-group "Internet" \
+    --linux-app-category "network" \
+    --linux-package-deps "firefox|chromium|brave|firefox-esr|librewolf|icecat" \
+    --linux-shortcut \
     --name i2pbrowser \
     --app-version "$GITHUB_TAG" \
     --input src/build \
