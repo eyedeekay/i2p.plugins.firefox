c79018a29a507930ff7fa2ac487aecf25cd0e55f
idk <hankhill19580@gmail.com>
2022-09-06 19:35:19 -0400

    fedora fixes

--- fedora.sh
+++ fedora.sh
@@ -4,4 +4,4 @@
 docker build -t eyedeekay/i2p.plugins.firefox .
 docker rm -f i2pbrowser-fedora
 docker run --name i2pbrowser-fedora eyedeekay/i2p.plugins.firefox
-docker cp i2pbrowser-fedora:/src/i2p.plugins.firefox/i2pbrowser-${GITHUB_TAG}.x86_64.rpm .
+docker cp i2pbrowser-fedora:/src/i2p.plugins.firefox/i2pbrowser-${GITHUB_TAG}-1.x86_64.rpm .
