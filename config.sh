bce2f0e2867bb977725315fddceeb39491446369
idk <hankhill19580@gmail.com>
2022-09-05 21:53:24 -0400

    Windows releaser too.

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 export GITHUB_USER=eyedeekay
 export GITHUB_REPO=i2p.plugins.firefox
-export GITHUB_NAME="Always pass --new-instance to Firefox"
+export GITHUB_NAME="Add support for Debian and Windows packages"
 export GITHUB_DESCRIPTION=$(cat CHANGES.md)
-export GITHUB_TAG=0.0.27
+export GITHUB_TAG=0.0.28
