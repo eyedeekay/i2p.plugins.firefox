a179482b954cfd9fe5efd9862413780ab45a01ae
idk <hankhill19580@gmail.com>
2022-09-05 21:14:59 -0400

    release

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 
 . ./config.sh
-ant distclean
+ant distclean clangFmt
 ./javadoc.sh
 NUMLINE=`grep release.number build.xml | head -n 1`
 sed -i "s|$NUMLINE|        <property name=\"release.number\" value=\"$GITHUB_TAG\" />|g" build.xml
