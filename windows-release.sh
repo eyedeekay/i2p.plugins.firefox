6803280bf25dcc15c6c11787fd4346bafe6262d9
idk <hankhill19580@gmail.com>
2022-09-05 22:04:23 -0400

    fix windows releaser script
    Former-commit-id: a9e1135ef599964b278e98d39f9a4c39f6fe01c4
    Former-commit-id: 6d9e221fbb4e2b890435054d0a140b209f37a4e6

--- windows-release.sh
+++ windows-release.sh
@@ -9,7 +9,7 @@ github-release upload --user "${GITHUB_USER}" \
     --repo "${GITHUB_REPO}" \
     --tag "${GITHUB_TAG}" \
     --label "I2P Browser launcher as a Jpackage inside of an MSI package." \
-    --name "i2pbrowser_${GITHUB_TAG}.msi" \
-    --file "i2pbrowser_${GITHUB_TAG}.msi" \
+    --name "i2pbrowser-${GITHUB_TAG}.msi" \
+    --file "i2pbrowser-${GITHUB_TAG}.msi" \
     --replace
 echo "Uploaded Windows MSI package"
