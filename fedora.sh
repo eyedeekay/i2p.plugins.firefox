fdd3aac3913e239bb98f3c4362ad8a824913b11d
idk <hankhill19580@gmail.com>
2022-09-06 18:42:01 -0400

    container-build redhat packages

--- fedora.sh
+++ fedora.sh
@@ -1,16 +1,6 @@
 #! /usr/bin/env sh
 
 . ./config.sh
-jpackage --verbose \
-    --type rpm \
-    --linux-deb-maintainer hankhill19580@gmail.com \
-    --linux-menu-group "Network;WebBrowser;P2P" \
-    --linux-app-category "Network" \
-    --linux-package-deps "firefox|chromium|brave|firefox-esr|librewolf|icecat" \
-    --linux-shortcut \
-    --license-file LICENSE.md \
-    --name i2pbrowser \
-    --app-version "$GITHUB_TAG" \
-    --input src/build \
-    --main-jar i2pfirefox.jar \
-    --main-class net.i2p.i2pfirefox.I2PBrowser
+docker build -t eyedeekay/i2p.plugins.firefox .
+docker run --name i2pbrowser-fedora i2p.plugins.firefox
+docker cp i2pbrowser-fedora:/src/i2p.plugins.firefox/i2pbrowser-${GITHUB_TAG}.rpm
