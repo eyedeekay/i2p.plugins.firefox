ae12ba9a6a0537b85c7440db3584ecea4098ff2a
idk <hankhill19580@gmail.com>
2022-09-07 14:15:31 -0400

    update packages.sh script
    Former-commit-id: 207d7ffcd062b619f155ac2a04919207efeec604
    Former-commit-id: 4a3b5f53669ffe64764e7c08fcc683d6aa68f65e

--- packages.sh
+++ packages.sh
@@ -19,12 +19,12 @@ echo "Downloaded jpackage zip"
 github-release download --user "${GITHUB_USER}" \
     --repo "${GITHUB_REPO}" \
     --tag "${GITHUB_TAG}" \
-    --name "i2pbrowser_${GITHUB_TAG}_.deb"
+    --name "i2pbrowser_${GITHUB_TAG}_amd64.deb"
 echo "Downloaded debian package"
 github-release download --user "${GITHUB_USER}" \
     --repo "${GITHUB_REPO}" \
     --tag "${GITHUB_TAG}" \
-    --name "i2pbrowser-${GITHUB_TAG}_.rpm"
+    --name "i2pbrowser-${GITHUB_TAG}-1.x86_64.rpm"
 echo "Downloaded fedora package"
 github-release download --user "${GITHUB_USER}" \
     --repo "${GITHUB_REPO}" \
@@ -49,7 +49,7 @@ echo "- [$(sha256sum i2pfirefox.jar)](https://github.com/${GITHUB_USER}/${GITHUB
 echo "- [$(sha256sum i2pfirefox.zip)](https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/download/${GITHUB_TAG}/i2pfirefox.zip)" | tee -a RELEASES.md
 echo "- [$(sha256sum i2pbrowser.zip)](https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/download/${GITHUB_TAG}/i2pbrowser.zip)" | tee -a RELEASES.md
 echo "- [$(sha256sum i2pbrowser_${GITHUB_TAG}_amd64.deb)](https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/download/${GITHUB_TAG}/i2pbrowser_${GITHUB_TAG}_amd64.deb)" | tee -a RELEASES.md
-echo "- [$(sha256sum i2pbrowser-${GITHUB_TAG}-x86_64.rpm)](https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/download/${GITHUB_TAG}/i2pbrowser-${GITHUB_TAG}-1.x86_64.rpm.rpm)" | tee -a RELEASES.md
+echo "- [$(sha256sum i2pbrowser-${GITHUB_TAG}-1.x86_64.rpm)](https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/download/${GITHUB_TAG}/i2pbrowser-${GITHUB_TAG}-1.x86_64.rpm)" | tee -a RELEASES.md
 echo "- [$(sha256sum i2pbrowser.msi)](https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/download/${GITHUB_TAG}/i2pbrowser.msi)" | tee -a RELEASES.md
 echo "- [$(sha256sum i2pbrowser.exe)](https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/download/${GITHUB_TAG}/i2pbrowser.exe)" | tee -a RELEASES.md
 echo "- [$(sha256sum i2pbrowser-portable.zip)](https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/download/${GITHUB_TAG}/i2pbrowser-portable.zip)" | tee -a RELEASES.md
