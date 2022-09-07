6042f30350198700a683223c8c6e683c05f2076b
idk <hankhill19580@gmail.com>
2022-09-06 22:40:52 -0400

    update index.html

--- RELEASES.md
+++ RELEASES.md
@@ -1,12 +1,12 @@
 Sums for release 0.0.30
 ==============================
 
-[74c272422f1b1753ee02e0cc5bee22e74a740ac4c929343559377a265825586e  i2pfirefox.jar](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/0.0.30/i2pfirefox.jar)
-[7e387b08f6871c5465a6d6c554afd175889dd9e1eabd5d33a7c62189f4db1c3c  i2pfirefox.zip](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/0.0.30/i2pfirefox.zip)
-[4befe1bbd83c4652ee7253c3a9c6023f8f3381c85a7565412e9d5136d03cac07  i2pbrowser.zip](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/0.0.30/i2pbrowser.zip)
-[96c1fc9b8e9ed715737e7cfa58761d6ef981c15a0eb3c75c3ac0843ce995c604  i2pbrowser_0.0.30_.deb](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/0.0.30/i2pbrowser_0.0.30_.deb)
-[b8c45ded657ac2e8731364fbaa68029a71db86cf97d68ca318348eecd7fd61cf  i2pbrowser-0.0.30_.rpm](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/0.0.30/i2pbrowser-0.0.30_.rpm)
-[d5848aa6d0be92a1e248c8e25a2396a720dfdd3bf68b73b77cd92163b6945fbf  i2pbrowser.msi](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/0.0.30/i2pbrowser.msi)
-[54032b5cee012dab75dcdf4c935bd5528c0bee50ea0deb178ec43fc47125edf1  i2pbrowser.exe](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/0.0.30/i2pbrowser.exe)
-[4eb1ee90373bf983421ba6c568cdd2b1ca88f838d64540d071f98b9c597c3c09  i2pbrowser-portable.zip](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/0.0.30/i2pbrowser-portable.zip)
+- [74c272422f1b1753ee02e0cc5bee22e74a740ac4c929343559377a265825586e  i2pfirefox.jar](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/0.0.30/i2pfirefox.jar)
+- [7e387b08f6871c5465a6d6c554afd175889dd9e1eabd5d33a7c62189f4db1c3c  i2pfirefox.zip](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/0.0.30/i2pfirefox.zip)
+- [4befe1bbd83c4652ee7253c3a9c6023f8f3381c85a7565412e9d5136d03cac07  i2pbrowser.zip](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/0.0.30/i2pbrowser.zip)
+- [96c1fc9b8e9ed715737e7cfa58761d6ef981c15a0eb3c75c3ac0843ce995c604  i2pbrowser_0.0.30_.deb](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/0.0.30/i2pbrowser_0.0.30_.deb)
+- [b8c45ded657ac2e8731364fbaa68029a71db86cf97d68ca318348eecd7fd61cf  i2pbrowser-0.0.30_.rpm](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/0.0.30/i2pbrowser-0.0.30_.rpm)
+- [d5848aa6d0be92a1e248c8e25a2396a720dfdd3bf68b73b77cd92163b6945fbf  i2pbrowser.msi](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/0.0.30/i2pbrowser.msi)
+- [54032b5cee012dab75dcdf4c935bd5528c0bee50ea0deb178ec43fc47125edf1  i2pbrowser.exe](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/0.0.30/i2pbrowser.exe)
+- [4eb1ee90373bf983421ba6c568cdd2b1ca88f838d64540d071f98b9c597c3c09  i2pbrowser-portable.zip](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/0.0.30/i2pbrowser-portable.zip)
 
--- packages.sh
+++ packages.sh
@@ -45,17 +45,17 @@ echo "Downloaded Windows ZIP package"
 echo "Sums for release ${GITHUB_TAG}" | tee RELEASES.md
 echo "==============================" | tee -a RELEASES.md
 echo "" | tee -a RELEASES.md
-echo "[$(sha256sum i2pfirefox.jar)](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pfirefox.jar)" | tee -a RELEASES.md
-echo "[$(sha256sum i2pfirefox.zip)](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pfirefox.zip)" | tee -a RELEASES.md
-echo "[$(sha256sum i2pbrowser.zip)](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pbrowser.zip)" | tee -a RELEASES.md
-echo "[$(sha256sum i2pbrowser_${GITHUB_TAG}_.deb)](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pbrowser_${GITHUB_TAG}_.deb)" | tee -a RELEASES.md
-echo "[$(sha256sum i2pbrowser-${GITHUB_TAG}_.rpm)](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pbrowser-${GITHUB_TAG}_.rpm)" | tee -a RELEASES.md
-echo "[$(sha256sum i2pbrowser.msi)](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pbrowser.msi)" | tee -a RELEASES.md
-echo "[$(sha256sum i2pbrowser.exe)](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pbrowser.exe)" | tee -a RELEASES.md
-echo "[$(sha256sum i2pbrowser-portable.zip)](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pbrowser-portable.zip)" | tee -a RELEASES.md
+echo "- [$(sha256sum i2pfirefox.jar)](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pfirefox.jar)" | tee -a RELEASES.md
+echo "- [$(sha256sum i2pfirefox.zip)](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pfirefox.zip)" | tee -a RELEASES.md
+echo "- [$(sha256sum i2pbrowser.zip)](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pbrowser.zip)" | tee -a RELEASES.md
+echo "- [$(sha256sum i2pbrowser_${GITHUB_TAG}_.deb)](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pbrowser_${GITHUB_TAG}_.deb)" | tee -a RELEASES.md
+echo "- [$(sha256sum i2pbrowser-${GITHUB_TAG}_.rpm)](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pbrowser-${GITHUB_TAG}_.rpm)" | tee -a RELEASES.md
+echo "- [$(sha256sum i2pbrowser.msi)](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pbrowser.msi)" | tee -a RELEASES.md
+echo "- [$(sha256sum i2pbrowser.exe)](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pbrowser.exe)" | tee -a RELEASES.md
+echo "- [$(sha256sum i2pbrowser-portable.zip)](https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pbrowser-portable.zip)" | tee -a RELEASES.md
 echo "" | tee -a RELEASES.md
 
 git add RELEASES.md
 edgar
-git commit -am "Update packages page"
+git commit -am "Update releases page"
 git push --all
