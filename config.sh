946bcb1348e4c9a843ac550870eb4948ecf4940d
idk <hankhill19580@gmail.com>
2022-09-07 03:50:51 -0400

    fix url argument passing
    Former-commit-id: b2f604a4a511a91cc8e895d2b07ef03c0e1c26aa
    Former-commit-id: 45388e5d724cf78a8693c8b846b4fc5ff0e44d51

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 export GITHUB_USER=eyedeekay
 export GITHUB_REPO=i2p.plugins.firefox
-export GITHUB_NAME="Destroy headless process if it goes too long"
+export GITHUB_NAME="Fix URL argument passing"
 export GITHUB_DESCRIPTION=$(cat CHANGES.md)
-export GITHUB_TAG=0.0.31
+export GITHUB_TAG=0.0.32
