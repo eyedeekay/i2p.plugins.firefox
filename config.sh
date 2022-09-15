638e880dc4f5c026454cb4d7b3a7840e3bba9e98
idk <hankhill19580@gmail.com>
2022-09-14 23:37:29 -0400

    add descriptions

--- config.sh
+++ config.sh
@@ -2,5 +2,5 @@
 export GITHUB_USER=eyedeekay
 export GITHUB_REPO=i2p.plugins.firefox
 export GITHUB_NAME="Update Extension Downloads"
-export GITHUB_DESCRIPTION=$(cat CHANGES.md)
+export GITHUB_DESCRIPTION=$(cat CHANGES.md VERSION.md)
 export GITHUB_TAG=0.0.37
