e68c5b956868753a91ed78f174f3c7b4a1e86778
idk <hankhill19580@gmail.com>
2022-09-16 16:39:05 -0400

    dockerize

--- fedora.sh
+++ fedora.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 
 . ./config.sh
-docker build -f Dockerfile.fedora -t eyedeekay/i2p.plugins.firefox .
+docker build -f Dockerfile.fedora -t eyedeekay/i2p.plugins.firefox.fedora .
 docker rm -f i2pbrowser-fedora
-docker run --name i2pbrowser-fedora eyedeekay/i2p.plugins.firefox
+docker run --name i2pbrowser-fedora eyedeekay/i2p.plugins.firefox.fedora
 docker cp i2pbrowser-fedora:/src/i2p.plugins.firefox/i2pbrowser-${GITHUB_TAG}-1.x86_64.rpm .
