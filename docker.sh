6abad05a1846ce360430b26c48d80561bc49e393
idk <hankhill19580@gmail.com>
2022-09-16 17:57:53 -0400

    set display variable correctly in docker.sh

--- docker.sh
+++ docker.sh
@@ -6,6 +6,6 @@ docker build -t eyedeekay/i2p.plugins.firefox .
 xhost + local:docker
 docker run -it --rm \
         --net=host \
-        -e DISPLAY=unix$(DISPLAY) \
+        -e DISPLAY=unix"${DISPLAY}" \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
         eyedeekay/i2p.plugins.firefox
