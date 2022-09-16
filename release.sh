6111c1a1a605975109c9db5bd77941bb5507b636
idk <hankhill19580@gmail.com>
2022-09-16 00:00:18 -0400

    move prerelease scripts

--- release.sh
+++ release.sh
@@ -1,14 +1,7 @@
 #! /usr/bin/env sh
 
 . ./config.sh
-ant distclean clangFmt
-./javadoc.sh
-NUMLINE=`grep release.number build.xml | head -n 1`
-READMELINE=`grep 'i2p.plugins.firefox/releases/download' README.md`
-sed -i "s|${READMELINE}|wget https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pfirefox.zip|g" README.md
-sed -i "s|${NUMLINE}|        <property name=\"release.number\" value=\"$GITHUB_TAG\" />|g" build.xml
-edgar && git push --all
-ant jar freeZip jpackage debian fedora
+./preprelease.sh
 profilezipsum=$(sha256sum src/i2p.chromium.base.profile.zip)
 github-release upload --user "${GITHUB_USER}" \
     --repo "${GITHUB_REPO}" \
