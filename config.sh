7bdc984a5e81609933be33f9aa1f92e64f0451dc
idk <hankhill19580@gmail.com>
2022-09-16 00:01:49 -0400

    update PACKAGES.html

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 export GITHUB_USER=eyedeekay
 export GITHUB_REPO=i2p.plugins.firefox
-export GITHUB_NAME="Update Extension Downloads"
+export GITHUB_NAME="Add app-mode for local-only browsing"
 export GITHUB_DESCRIPTION=$(cat CHANGES.md VERSION.md)
 export GITHUB_TAG=0.0.37
