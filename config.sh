ff6e37620ce0f53b8143ddbcb4c82cef2df7a59c
idk <hankhill19580@gmail.com>
2022-10-01 18:05:51 -0400

    bump version
    Former-commit-id: 2cd46a1f6de9c8f73e456828b1fa10dad8ce0795
    Former-commit-id: fd46e6bb7268086ef7985024bbebc4fa8b6cfef2

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 export GITHUB_USER=eyedeekay
 export GITHUB_REPO=i2p.plugins.firefox
-export GITHUB_NAME="Add app-mode for local-only browsing, buildsystem improvements"
+export GITHUB_NAME="Make OSX support as good as everywhere else"
 export GITHUB_DESCRIPTION=$(cat CHANGES.md VERSION.md)
-export GITHUB_TAG=1.0.0
+export GITHUB_TAG=1.0.1
