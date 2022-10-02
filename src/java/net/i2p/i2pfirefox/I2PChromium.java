0864d1860a064884698bcfa94a59b527e053abb4
idk <hankhill19580@gmail.com>
2022-10-01 22:25:24 -0400

    remove unused variable from chromium wrapper

--- src/java/net/i2p/i2pfirefox/I2PChromium.java
+++ src/java/net/i2p/i2pfirefox/I2PChromium.java
@@ -511,7 +511,6 @@ public class I2PChromium extends I2PCommonBrowser {
         }
       }
       if (isOSX()) {
-        String argString = join(Arrays.copyOfRange(newArgs, 1, newArgs.length));
         String[] fg = {""};
         String[] lastArgs =
             Stream.concat(Arrays.stream(newArgs), Arrays.stream(fg))
