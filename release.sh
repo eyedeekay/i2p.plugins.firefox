f909f770e8c60849ca7a3541c8af5496b58ff4a6
idk <hankhill19580@gmail.com>
2022-10-05 14:51:54 -0400

    add source tarballs to releases too, don't allow release.sh to replace release files if they are unchanged

--- release.sh
+++ release.sh
@@ -14,14 +14,20 @@ github-release edit --user "${GITHUB_USER}" \
     --description "${GITHUB_DESCRIPTION}" \
     --tag "${GITHUB_TAG}"; true
 echo "Relase ${GITHUB_TAG} setup"
+tarsum=$(sha256sum i2p.plugins.firefox.tar.gz)
+github-release upload --user "${GITHUB_USER}" \
+    --repo "${GITHUB_REPO}" \
+    --tag "${GITHUB_TAG}" \
+    --label "I2P Browser launcher source tarball. ${tarsum}" \
+    --name "i2p.plugins.firefox-${GITHUB_TAG}.tar.gz" \
+    --file "i2p.plugins.firefox.tar.gz" 
 jarsum=$(sha256sum "src/build/i2pfirefox.jar")
 github-release upload --user "${GITHUB_USER}" \
     --repo "${GITHUB_REPO}" \
     --tag "${GITHUB_TAG}" \
     --label "I2P Browser launcher as a .jar. Useful as a library or for advanced users. ${jarsum}" \
     --name "i2pfirefox.jar" \
-    --file "src/build/i2pfirefox.jar" \
-    --replace
+    --file "src/build/i2pfirefox.jar" 
 echo "Uploaded jar"
 zipsum=$(sha256sum "i2pfirefox.zip")
 github-release upload --user "${GITHUB_USER}" \
@@ -29,8 +35,7 @@ github-release upload --user "${GITHUB_USER}" \
     --tag "${GITHUB_TAG}" \
     --label "I2P Browser launcher as a .jar and a set of semi-univeral launcher scripts. ${zipsum}" \
     --name "i2pfirefox.zip" \
-    --file "i2pfirefox.zip" \
-    --replace
+    --file "i2pfirefox.zip" 
 echo "Uploaded freestanding zip"
 jvmsum=$(sha256sum "i2pbrowser.zip")
 github-release upload --user "${GITHUB_USER}" \
@@ -38,8 +43,7 @@ github-release upload --user "${GITHUB_USER}" \
     --tag "${GITHUB_TAG}" \
     --label "I2P Browser launcher as a Jpackage, does not require a JVM. ${jvmsum}" \
     --name "i2pbrowser.zip" \
-    --file "i2pbrowser.zip" \
-    --replace
+    --file "i2pbrowser.zip" 
 echo "Uploaded jpackage zip"
 debsum=$(sha256sum "i2pbrowser_${GITHUB_TAG}_amd64.deb")
 github-release upload --user "${GITHUB_USER}" \
@@ -47,8 +51,7 @@ github-release upload --user "${GITHUB_USER}" \
     --tag "${GITHUB_TAG}" \
     --label "I2P Browser launcher as a Jpackage inside of a Debian package. ${debsum}" \
     --name "i2pbrowser_${GITHUB_TAG}_amd64.deb" \
-    --file "i2pbrowser_${GITHUB_TAG}_amd64.deb" \
-    --replace
+    --file "i2pbrowser_${GITHUB_TAG}_amd64.deb" 
 echo "Uploaded debian package"
 rpmsum=$(sha256sum i2pbrowser-${GITHUB_TAG}-1.x86_64.rpm)
 github-release upload --user "${GITHUB_USER}" \
@@ -56,8 +59,7 @@ github-release upload --user "${GITHUB_USER}" \
     --tag "${GITHUB_TAG}" \
     --label "I2P Browser launcher as a Jpackage inside of a Fedora package. ${rpmsum}" \
     --name "i2pbrowser-${GITHUB_TAG}-1.x86_64.rpm" \
-    --file "i2pbrowser-${GITHUB_TAG}-1.x86_64.rpm" \
-    --replace
+    --file "i2pbrowser-${GITHUB_TAG}-1.x86_64.rpm" 
 echo "Uploaded fedora package"
 
 git pull github --tags
