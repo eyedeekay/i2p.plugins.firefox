bea5c699ec141a37288798a5b3f0e05b1c27556f
idk <hankhill19580@gmail.com>
2022-10-20 00:46:05 -0400

    update version
    Former-commit-id: b192c8fea510abd0aff04b1bd6c7a3929f4df35a
    Former-commit-id: 6fb57c41d820434ed79757aa1f2d20d972c4354e

--- config.sh
+++ config.sh
@@ -3,4 +3,4 @@ export GITHUB_USER=eyedeekay
 export GITHUB_REPO=i2p.plugins.firefox
 export GITHUB_NAME="Completes features for PWA-Like mode by flattening UI onto one toolbar"
 export GITHUB_DESCRIPTION=$(cat CHANGES.md VERSION.md)
-export GITHUB_TAG=1.0.1
+export GITHUB_TAG=1.0.2
