8ffd4025efc5d46a73de77d9f3637158acb5110d
idk <hankhill19580@gmail.com>
2022-10-23 11:34:25 -0400

    Add config.md

--- browser.config.sh
+++ browser.config.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 
-rm browser.config
+rm -f browser.config
 ant clangFmt jar
 java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -chromium -outputconfig -nosystray "http://idk.i2p"
-#java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -firefox -outputconfig -nosystray
-#java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -generic -outputconfig -nosystray
+java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -firefox -outputconfig -nosystray "http://idk.i2p"
+java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -generic -outputconfig -nosystray "http://idk.i2p"
