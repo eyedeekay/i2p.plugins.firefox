1d54ea83587f2ae79cb65e0b3abc8747a8042466
idk <hankhill19580@gmail.com>
2022-10-24 00:10:47 -0400

    update index.html

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 export GITHUB_USER=eyedeekay
 export GITHUB_REPO=i2p.plugins.firefox
-export GITHUB_NAME="Completes features for PWA-Like mode by flattening UI onto one toolbar"
+export GITHUB_NAME="Tweaks to how the Tor Browser integration works"
 export GITHUB_DESCRIPTION=$(cat CHANGES.md VERSION.md)
-export GITHUB_TAG=1.0.2
+export GITHUB_TAG=1.0.3
--- preprelease.sh
+++ preprelease.sh
@@ -6,6 +6,8 @@ ant distclean clangFmt versionMd
 NUMLINE=`grep release.number build.xml | head -n 1`
 READMELINE=`grep 'i2p.plugins.firefox/releases/download' README.md | grep i2pfirefox`
 sed -i "s|${READMELINE}|wget https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pfirefox.zip|g" README.md
+READMELINE=`grep 'i2p.plugins.firefox/releases/download' LINUX.md | grep i2pbrowser`
+sed -i "s|${READMELINE}|wget https://github.com/eyedeekay/i2p.plugins.firefox/releases/download/${GITHUB_TAG}/i2pbrowser.zip|g" LINUX.md
 sed -i "s|${NUMLINE}|        <property name=\"release.number\" value=\"$GITHUB_TAG\" />|g" build.xml
 edgar && git push --all
 ant distclean tarball versionMd jar freeZip jpackage debian fedora
