d95237d5e6bc22dbfe083a15b978b7e71e0098b3
idk <hankhill19580@gmail.com>
2022-10-24 00:06:27 -0400

    update gitignore, shift testing around a bit
    Former-commit-id: 38f65fffe0c70132f9d1214c4e41e7403033d933
    Former-commit-id: 0512bcb79ca62ba18f37467908c99f3b5977faca

--- test.sh
+++ test.sh
@@ -15,71 +15,9 @@ echo "Testing auto-selector with remote AND local URL parameter."
 java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray "http://127.0.0.1:7657" "http://idk.i2p" 2> auto.3.err 1> auto.3.log
 echo "Testing auto-selector with private browsing parameter"
 java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -private "http://127.0.0.1:7657" 2> auto.4.err 1> auto.4.log
-
 echo "Auto-Selector tests completed"
-sleep 2s
-rm -rf i2p.chromium.base.profile i2p.chromium.profile
 
-echo "Testing Chromium with no private and no URL parameters."
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -chromium 2> chrome.1.err 1> chrome.1.log
-echo "Testing Chromium with local URL parameter."
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -chromium "http://127.0.0.1:7657" 2> chrome.1.err 1> chrome.1.log
-echo "Testing Chromium with remote URL parameter."
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -chromium "http://idk.i2p" 2> chrome.2.err 1> chrome.2.log
-echo "Testing Chromium with remote AND local URL parameter."
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -chromium "http://127.0.0.1:7657" "http://idk.i2p" 2> fox.3.err 1> fox.3.log
-echo "Testing Chromium with private browsing parameter"
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -chromium -private "http://127.0.0.1:7657" 2> chrome.4.err 1> chrome.4.log
-
-echo "Chromium tests completed"
-sleep 2s
-rm -rf i2p.chromium.base.profile i2p.chromium.profile
-
-echo "Testing Chromium with no private and no URL parameters."
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -chromium -usability 2> chrome.1.err 1> chrome.1.log
-echo "Testing Chromium with local URL parameter."
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -chromium -usability "http://127.0.0.1:7657" 2> chrome.1.err 1> chrome.1.log
-echo "Testing Chromium with remote URL parameter."
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -chromium -usability "http://idk.i2p" 2> chrome.2.err 1> chrome.2.log
-echo "Testing Chromium with remote AND local URL parameter."
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -chromium -usability "http://127.0.0.1:7657" "http://idk.i2p" 2> fox.3.err 1> fox.3.log
-echo "Testing Chromium with private browsing parameter"
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -chromium -usability -private "http://127.0.0.1:7657" 2> chrome.4.err 1> chrome.4.log
-
-echo "Chromium Usability tests completed"
-sleep 2s
-
-rm -rf i2p.firefox.base.profile i2p.firefox.profile
-
-echo "Testing Firefox with no private and no URL parameters."
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -firefox 2> fox.0.err 1> fox.0.log
-echo "Testing Firefox with local URL parameter."
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -firefox "http://127.0.0.1:7657" 2> fox.1.err 1> fox.1.log
-echo "Testing Firefox with remote URL parameter."
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -firefox "http://idk.i2p" 2> fox.2.err 1> fox.2.log
-echo "Testing Firefox with remote AND local URL parameter."
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -firefox "http://127.0.0.1:7657" "http://idk.i2p" 2> fox.3.err 1> fox.3.log
-echo "Testing Firefox with private browsing parameter"
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -firefox -private "http://127.0.0.1:7657" 2> fox.4.err 1> fox.4.log
-
-echo "Firefox tests completed"
-sleep 2s
-rm -rf i2p.firefox.usability.profile i2p.firefox.profile
-
-echo "Testing Firefox with no private and no URL parameters."
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -firefox -usability 2> fox.0.err 1> fox.0.log
-echo "Testing Firefox with local URL parameter."
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -firefox -usability "http://127.0.0.1:7657" 2> fox.1.err 1> fox.1.log
-echo "Testing Firefox with remote URL parameter."
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -firefox -usability "http://idk.i2p" 2> fox.2.err 1> fox.2.log
-echo "Testing Firefox with remote AND local URL parameter."
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -firefox -usability "http://127.0.0.1:7657" "http://idk.i2p" 2> fox.3.err 1> fox.3.log
-echo "Testing Firefox with private browsing parameter"
-java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -firefox -usability -private "http://127.0.0.1:7657" 2> fox.4.err 1> fox.4.log
-
-echo "Firefox Usability-Mode tests completed"
-sleep 2s
-rm -rf i2p.chromium.base.profile i2p.chromium.profile i2p.firefox.base.profile i2p.firefox.profile
+rm -rf i2p.chromium.base.profile i2p.chromium.profile i2p.firefox.base.profile i2p.firefox.profile i2p.firefox.usability.profile
 
 echo "Testing UNSAFE auto-selector with no private and no URL parameters."
 java -cp ./src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PBrowser -nosystray -generic 2> gen.0.err 1> gen.0.log
