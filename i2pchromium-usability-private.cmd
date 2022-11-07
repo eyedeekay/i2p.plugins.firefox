bd230b963898d60797db8d1d8244d0fe16dd769c
idk <hankhill19580@gmail.com>
2022-11-06 21:45:42 -0500

    update release

--- i2pchromium-usability-private.cmd
+++
@@ -1,3 +0,0 @@
-:; dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd); java -cp "$dir"/src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PChromium -private -usability; exit $?
-@ECHO OFF
-java -cp %cd%/src/build/i2pfirefox.jar net.i2p.i2pfirefox.I2PChromium -private -usability
