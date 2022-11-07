d9b996a9220f6d9110cabbd948a4589779dfcbd2
idk <hankhill19580@gmail.com>
2022-11-06 21:55:54 -0500

    move javadocs to docs dir

--- javadoc.sh
+++ javadoc.sh
@@ -4,6 +4,6 @@ rm -rf docs
 
 ~/.cargo/bin/lojidoc src/java/net/i2p/i2pfirefox/ -s -l > report.log
 ~/.cargo/bin/lojidoc -c src/java/net/i2p/i2pfirefox/ -s
-mv -v generated/net/i2p/i2pfirefox/*.md .
+mv -v generated/net/i2p/i2pfirefox/*.md ./docs/
 rm -rf generated
 git add ./*.md
