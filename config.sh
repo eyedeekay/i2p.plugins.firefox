7041cca7c9b4fa69039b93080c09840e94b968ef
idk <hankhill19580@gmail.com>
2022-11-22 23:55:19 -0500

    update changelog

--- config.sh
+++ config.sh
@@ -3,4 +3,4 @@ export GITHUB_USER=eyedeekay
 export GITHUB_REPO=i2p.plugins.firefox
 export GITHUB_NAME="Enforces App and Browser Modes"
 export GITHUB_DESCRIPTION=$(cat CHANGES.md VERSION.md)
-export GITHUB_TAG=1.0.4
+export GITHUB_TAG=1.0.5
