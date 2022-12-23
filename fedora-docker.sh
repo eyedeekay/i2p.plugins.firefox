125ed9bdda6ab169ca4991c483febeee4d6d7abf
idk <hankhill19580@gmail.com>
2022-12-23 00:37:28 +0000

    switch to caching more packages in docker build

--- fedora-docker.sh
+++ fedora-docker.sh
@@ -2,16 +2,8 @@
 
 . ./config.sh
 
-cat <<EOF > /etc/yum.repos.d/adoptium.repo
-[Adoptium]
-name=Adoptium
-baseurl=https://packages.adoptium.net/artifactory/rpm/${DISTRIBUTION_NAME:-$(. /etc/os-release; echo $ID)}/\$releasever/\$basearch
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.adoptium.net/artifactory/api/gpg/key/public
-EOF
-yum -y update
-yum -y install temurin-19-jdk
+dnf -y update
+dnf -y install temurin-19-jdk
 
 jpackage --verbose \
     --type rpm \
