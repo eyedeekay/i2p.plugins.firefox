bf0e479172a8c97a140d9537079fc95bece04e60
idk <hankhill19580@gmail.com>
2023-02-12 20:30:36 +0000

    bump version, update changelog

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 export GITHUB_USER=eyedeekay
 export GITHUB_REPO=i2p.plugins.firefox
-export GITHUB_NAME="Linux portable zips become tar.gz's, HTTPS Everywhere is completely deprecated, Linux portable zips default to Tor Browser"
+export GITHUB_NAME="Fixes release builds for Windows"
 export GITHUB_DESCRIPTION=$(cat CHANGES.md VERSION.md)
-export GITHUB_TAG=1.0.8
+export GITHUB_TAG=1.0.9
