375774687b9dc1b8805aaf50a759150cfa0ada8a
idk <idk@yoga7.idk.i2p>
2023-07-01 17:56:08 -0400

    update version

--- config.sh
+++ config.sh
@@ -3,4 +3,4 @@ export GITHUB_USER=eyedeekay
 export GITHUB_REPO=i2p.plugins.firefox
 export GITHUB_NAME="Fixes release builds for Windows"
 export GITHUB_DESCRIPTION=$(cat CHANGES.md VERSION.md)
-export GITHUB_TAG=1.0.9
+export GITHUB_TAG=1.1.0
