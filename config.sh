9f283c4ef5813f03e6a1f33fac9f61d5477dc453
eyedeekay <hankhill19580@gmail.com>
2024-08-21 00:38:00 -0400

    Use vendored crx3

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 export GITHUB_USER=eyedeekay
 export GITHUB_REPO=i2p.plugins.firefox
-export GITHUB_NAME="Updates extensions, plugin support temporarily removed(Stay on the old version)"
+export GITHUB_NAME="Fixes the build system"
 export GITHUB_DESCRIPTION=$(cat CHANGES.md VERSION.md)
 export GITHUB_TAG=$(git describe --tags --abbrev=0)
