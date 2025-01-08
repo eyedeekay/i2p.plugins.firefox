57c7d7d3418bb2ca429225e8630a20ca49bbfe18
eyedeekay <hankhill19580@gmail.com>
2025-01-07 20:54:13 -0500

    push with push.sh instead so that we get nightly builds

--- push.sh
+++ push.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env sh
 
 nightly_hash=nightly-$(git log -1 --format=%H)
-git tag -s "$nightly_hash"
+nightly_date=$(date)
+git tag -m "$nightly_date" -s "$nightly_hash"
 git push --all
 git push --tags
